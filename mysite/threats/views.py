d46c008e2875a5d615eb1d51af75fe14c8342fee
Hong-Yi Wang <hwang@vividsolutions.com>
2019-02-07 10:36:37 -0800

    Create threats service.

--- mysite/threats/views.py
+++ mysite/threats/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+import os
 
-# Create your views here.
+def index(request):
+	file = open(os.path.join(settings.BASE_DIR, 'threats.json'))
+	data = file.readlines()
+	file.close()
+	return HttpResponse(data, content_type='application/json')
