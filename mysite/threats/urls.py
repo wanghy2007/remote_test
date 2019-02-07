012de1d7c67a24a68329db21d7149a3f21196ea5
Hong-Yi Wang <hwang@vividsolutions.com>
2019-02-07 11:58:53 -0800

    Load data using AJAX.

--- mysite/threats/urls.py
+++ mysite/threats/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('data', views.data, name='data'),
     path('', views.index, name='index'),
 ]
--- mysite/threats/views.py
+++ mysite/threats/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.conf import settings
+from django.template import loader
 import os
 
-def index(request):
+def data(request):
 	file = open(os.path.join(settings.BASE_DIR, 'threats.json'))
 	data = file.readlines()
 	file.close()
 	return HttpResponse(data, content_type='application/json')
+
+def index(request):
+	template = loader.get_template('threats/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
